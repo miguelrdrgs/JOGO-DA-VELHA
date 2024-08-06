e8107dddddf84f3ebe413f8a2d0a87c7153c4ad6
miguel.rcoelho <miguel.rcoelho@ITA0557217W10-1>
2024-08-06 19:56:46 -0300

    Adicionar arquivos de projeto.

---
+++ JOGO DA VELHA/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace JOGO_DA_VELHA
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            BTN1 = new Button();
+            BTN2 = new Button();
+            BTN3 = new Button();
+            BTN6 = new Button();
+            BTN5 = new Button();
+            BTN4 = new Button();
+            BTN9 = new Button();
+            BTN8 = new Button();
+            BTN7 = new Button();
+            x = new Button();
+            O = new Button();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // BTN1
+            // 
+            BTN1.BackColor = Color.Black;
+            BTN1.ForeColor = Color.Transparent;
+            BTN1.Location = new Point(61, 111);
+            BTN1.Name = "BTN1";
+            BTN1.Size = new Size(117, 116);
+            BTN1.TabIndex = 0;
+            BTN1.UseVisualStyleBackColor = false;
+            BTN1.Click += BTN1_Click;
+            // 
+            // BTN2
+            // 
+            BTN2.BackColor = Color.Black;
+            BTN2.ForeColor = Color.Transparent;
+            BTN2.Location = new Point(176, 111);
+            BTN2.Name = "BTN2";
+            BTN2.Size = new Size(117, 116);
+            BTN2.TabIndex = 1;
+            BTN2.UseVisualStyleBackColor = false;
+            // 
+            // BTN3
+            // 
+            BTN3.BackColor = Color.Black;
+            BTN3.ForeColor = Color.Transparent;
+            BTN3.Location = new Point(289, 111);
+            BTN3.Name = "BTN3";
+            BTN3.Size = new Size(117, 116);
+            BTN3.TabIndex = 2;
+            BTN3.UseVisualStyleBackColor = false;
+            // 
+            // BTN6
+            // 
+            BTN6.BackColor = Color.Black;
+            BTN6.ForeColor = Color.Transparent;
+            BTN6.Location = new Point(289, 223);
+            BTN6.Name = "BTN6";
+            BTN6.Size = new Size(117, 116);
+            BTN6.TabIndex = 5;
+            BTN6.UseVisualStyleBackColor = false;
+            // 
+            // BTN5
+            // 
+            BTN5.BackColor = Color.Black;
+            BTN5.ForeColor = Color.Transparent;
+            BTN5.Location = new Point(176, 223);
+            BTN5.Name = "BTN5";
+            BTN5.Size = new Size(117, 116);
+            BTN5.TabIndex = 4;
+            BTN5.UseVisualStyleBackColor = false;
+            // 
+            // BTN4
+            // 
+            BTN4.BackColor = Color.Black;
+            BTN4.ForeColor = Color.Transparent;
+            BTN4.Location = new Point(61, 223);
+            BTN4.Name = "BTN4";
+            BTN4.Size = new Size(117, 116);
+            BTN4.TabIndex = 3;
+            BTN4.UseVisualStyleBackColor = false;
+            // 
+            // BTN9
+            // 
+            BTN9.BackColor = Color.Black;
+            BTN9.ForeColor = Color.Transparent;
+            BTN9.Location = new Point(289, 330);
+            BTN9.Name = "BTN9";
+            BTN9.Size = new Size(117, 116);
+            BTN9.TabIndex = 8;
+            BTN9.UseVisualStyleBackColor = false;
+            // 
+            // BTN8
+            // 
+            BTN8.BackColor = Color.Black;
+            BTN8.ForeColor = Color.Transparent;
+            BTN8.Location = new Point(176, 330);
+            BTN8.Name = "BTN8";
+            BTN8.Size = new Size(117, 116);
+            BTN8.TabIndex = 7;
+            BTN8.UseVisualStyleBackColor = false;
+            // 
+            // BTN7
+            // 
+            BTN7.BackColor = Color.Black;
+            BTN7.ForeColor = Color.Transparent;
+            BTN7.Location = new Point(61, 330);
+            BTN7.Name = "BTN7";
+            BTN7.Size = new Size(117, 116);
+            BTN7.TabIndex = 6;
+            BTN7.UseVisualStyleBackColor = false;
+            // 
+            // x
+            // 
+            x.BackColor = Color.Black;
+            x.ForeColor = Color.Transparent;
+            x.Location = new Point(134, 468);
+            x.Name = "x";
+            x.Size = new Size(75, 23);
+            x.TabIndex = 9;
+            x.Text = "X";
+            x.UseVisualStyleBackColor = false;
+            // 
+            // O
+            // 
+            O.BackColor = Color.Black;
+            O.ForeColor = Color.Transparent;
+            O.Location = new Point(256, 468);
+            O.Name = "O";
+            O.Size = new Size(75, 23);
+            O.TabIndex = 10;
+            O.Text = "O";
+            O.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.DarkKhaki;
+            pictureBox1.Location = new Point(12, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(443, 503);
+            pictureBox1.TabIndex = 11;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.DarkKhaki;
+            label1.Font = new Font("Showcard Gothic", 18F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.Location = new Point(148, 22);
+            label1.Name = "label1";
+            label1.Size = new Size(168, 30);
+            label1.TabIndex = 12;
+            label1.Text = "JOGO DA VEIA";
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveCaptionText;
+            button1.ForeColor = Color.Khaki;
+            button1.Location = new Point(105, 55);
+            button1.Name = "button1";
+            button1.Size = new Size(255, 31);
+            button1.TabIndex = 13;
+            button1.Text = "INICIAR JOGO";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(96F, 96F);
+            AutoScaleMode = AutoScaleMode.Dpi;
+            BackColor = SystemColors.ActiveBorder;
+            ClientSize = new Size(988, 575);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(O);
+            Controls.Add(x);
+            Controls.Add(BTN9);
+            Controls.Add(BTN8);
+            Controls.Add(BTN7);
+            Controls.Add(BTN6);
+            Controls.Add(BTN5);
+            Controls.Add(BTN4);
+            Controls.Add(BTN3);
+            Controls.Add(BTN2);
+            Controls.Add(BTN1);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button BTN1;
+        private Button BTN2;
+        private Button BTN3;
+        private Button BTN6;
+        private Button BTN5;
+        private Button BTN4;
+        private Button BTN9;
+        private Button BTN8;
+        private Button BTN7;
+        private Button x;
+        private Button O;
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Button button1;
+    }
+}
---
+++ JOGO DA VELHA/Form1.cs
@@ -0,0 +1,15 @@
+namespace JOGO_DA_VELHA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BTN1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
